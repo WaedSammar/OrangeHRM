4a33d6c6622c103a69a1ea2f273a712074558a4d
Waed Sammar <waedsammar12@gmail.com>
2025-04-17 17:17:11 +0300

    complete-login-tests

--- cypress/e2e/login_tests/login-spec.cy.ts
+++ cypress/e2e/login_tests/login-spec.cy.ts
@@ -1,14 +1,20 @@
-///<reference types = "cypress" />
+/// <reference types="cypress" />
+
+const correctUsername = 'Admin';
+const correctPassword = 'admin123';
+const wrongUsername = 'wrongUser';
+const wrongPassword = 'wrongPass';
+
 describe('Login Page Test Cases', () => {
 
   beforeEach(() => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/')
   });
 
   it('Login Test Successfully', () => {
 
-    cy.get("[name='username']").type("Admin")
-    cy.get("[name='password']").type("admin123")
+    cy.get("[name='username']").clear().type(correctUsername)
+    cy.get("[name='password']").clear().type(correctPassword)
     cy.get("[type='submit']").click()
 
     cy.get("img[alt='client brand banner']")
@@ -18,25 +24,34 @@ describe('Login Page Test Cases', () => {
     // cy.url().should('include', '/dashboard')
   });
 
+  it('Sensitive Username', () => {
+
+    cy.get("[name='username']").clear().type(correctUsername.toLocaleUpperCase())
+    cy.get("[name='password']").clear().type(correctPassword)
+    cy.get("[type='submit']").click()
+
+    cy.url().should('include', '/dashboard')
+  });
+
   it('Test wrong Password', () => {
-    cy.get("[name='username']").type("Admin")
-    cy.get("[name='password']").type("admin12345")
+    cy.get("[name='username']").clear().type(correctUsername)
+    cy.get("[name='password']").clear().type(wrongPassword)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials')
   });
 
   it('Test wrong Username', () => {
-    cy.get("[name='username']").type("AdminAdmin")
-    cy.get("[name='password']").type("admin123")
+    cy.get("[name='username']").clear().type(wrongUsername)
+    cy.get("[name='password']").clear().type(correctPassword)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials')
   });
 
   it('Test wrong Password and Username', () => {
-    cy.get("[name='username']").type("AdminAdmin")
-    cy.get("[name='password']").type("admin12345")
+    cy.get("[name='username']").clear().type(wrongUsername)
+    cy.get("[name='password']").clear().type(wrongPassword)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-alert-content-text').should('contain.text', 'Invalid credentials')
@@ -45,11 +60,14 @@ describe('Login Page Test Cases', () => {
   it('Test empty Username and Password', () => {
     cy.get("[type='submit']").click()
 
-    cy.get('.oxd-input-field-error-message').should('contain.text', 'Required')
+    cy.get('.oxd-input-field-error-message').should('have.length', 2)
+    cy.get('.oxd-input-field-error-message').eq(0).should('contain.text', 'Required')
+    cy.get('.oxd-input-field-error-message').eq(1).should('contain.text', 'Required')
+
   });
 
   it('Test empty Username with correct Password', () => {
-    cy.get("[name='password']").type("admin123")
+    cy.get("[name='password']").clear().type(correctUsername)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-input-field-error-message').should('have.length', 1)
@@ -57,7 +75,7 @@ describe('Login Page Test Cases', () => {
   });
 
   it('Test empty Username with wrong Password', () => {
-    cy.get("[name='password']").type("admin12345")
+    cy.get("[name='password']").clear().type(wrongPassword)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-input-field-error-message').should('have.length', 1)
@@ -65,7 +83,7 @@ describe('Login Page Test Cases', () => {
   });
 
   it('Test empty Password with correct Username', () => {
-    cy.get("[name='username']").type("Admin")
+    cy.get("[name='username']").clear().type(correctUsername)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-input-field-error-message').should('have.length', 1)
@@ -73,11 +91,15 @@ describe('Login Page Test Cases', () => {
   });
 
   it('Test empty Password with wrong Username', () => {
-    cy.get("[name='username']").type("AdminAdmin")
+    cy.get("[name='username']").clear().type(wrongUsername)
     cy.get("[type='submit']").click()
 
     cy.get('.oxd-input-field-error-message').should('have.length', 1)
     cy.get(".oxd-input-field-error-message").first().should('contain.text', 'Required')
+  });
+
+  it('Hidden Password', () => {
+    cy.get("[name='password']").should('have.attr', 'type', 'password')
   });
 
 });
