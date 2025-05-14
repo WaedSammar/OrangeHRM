1812ff74f696dd73857208bd6ecb1c932bd59383
Mohammad Abohasan <mabohasan@apple.com>
2025-05-14 18:04:42 +0300

    feat! add some  cypress cofigs

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
+    specPattern: "cypress/e2e/**/*-spec.cy.ts",
+    defaultBrowser: "chrome",
   },
   viewportWidth: 1728,
   viewportHeight: 1117,
