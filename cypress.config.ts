b30f4b6e38ba20a244506544204ee05cf81a6150
Mohammad Abohasan <mabohasan@apple.com>
2025-05-18 16:24:51 +0300

    feat! support cypress cloud

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "zwebt5",
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
     specPattern: "cypress/e2e/**/*-spec.cy.ts",
