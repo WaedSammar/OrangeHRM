95d4677110a777164f28cd2ebb1cc9041139b65d
Waed Sammar <waedsammar12@gmail.com>
2025-05-22 09:52:07 +0300

    feature! adding employee via API

--- cypress/e2e/pim-tests/pim-spec.cy.ts
+++ cypress/e2e/pim-tests/pim-spec.cy.ts
@@ -49,4 +49,73 @@ describe("Employee management - Add and Save Test Cases", () => {
     MyInfo.goToMyInfoPage();
     PIMPage.verifyEmployeeInfo(employeeInfo);
   });
+
+  it("Adding employee via API", () => {
+    cy.request({
+      method: "POST",
+      url: `/web/index.php/api/v2/pim/employees`,
+      body: {
+        firstName: employeeInfo.firstName,
+        middleName: employeeInfo.middleName,
+        lastName: employeeInfo.lastName,
+        employeeId: employeeInfo.employeeId,
+      },
+    }).then((response) => {
+      cy.log(response.body);
+      expect(response.status).to.eq(200);
+      const empNumber = response.body.data.empNumber;
+
+      cy.request({
+        method: "POST",
+        url: `/web/index.php/api/v2/admin/users`,
+        body: {
+          username: employeeInfo.userName,
+          password: employeeInfo.password,
+          status: true,
+          userRoleId: 2,
+          empNumber: empNumber,
+        },
+      }).then((response) => {
+        cy.log(response.body);
+        expect(response.status).to.eq(200);
+      });
+
+      cy.request({
+        method: "PUT",
+        url: `/web/index.php/api/v2/pim/employees/${empNumber}/personal-details`,
+        body: {
+          firstName: employeeInfo.firstName,
+          middleName: employeeInfo.middleName,
+          lastName: employeeInfo.lastName,
+          employeeId: employeeInfo.employeeId,
+          otherId: employeeInfo.otherId,
+          drivingLicenseNo: employeeInfo.licenseNum,
+          drivingLicenseExpiredDate: employeeInfo.expDate,
+          birthday: employeeInfo.dateOfBirth,
+          gender: 1,
+          maritalStatus: employeeInfo.maritalState,
+          nationalityId: 27,
+        },
+      }).then((response) => {
+        cy.log(response.body);
+        expect(response.status).to.eq(200);
+      });
+
+      cy.request({
+        method: "PUT",
+        url: `/web/index.php/api/v2/pim/employees/${empNumber}/custom-fields`,
+        body: {
+          custom1: employeeInfo.bloodType,
+          custom2: employeeInfo.testField,
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+
+      ElementHandler.logout();
+      cy.login(employeeInfo.userName, employeeInfo.password);
+      MyInfo.goToMyInfoPage();
+      PIMPage.verifyEmployeeInfo(employeeInfo);
+    });
+  });
 });
