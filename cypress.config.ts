9d292ab301963e0f12e7a81e4648db3bdda807ca
Waed Sammar <waedsammar12@gmail.com>
2025-05-30 19:49:02 +0300

    refactor! adding async await for code node

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "cypress";
 import * as xlsx from "xlsx";
+import { promises as fs } from "fs";
 
 export default defineConfig({
   projectId: "zwebt5",
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       on("task", {
-        parseXlsxToJson({ filePath }) {
-          const workbook = xlsx.readFile(filePath);
-          const result = {};
+        async parseXlsxToJson({ filePath }) {
+          await fs.access(filePath, fs.constants.R_OK);
+
+          const buffer = await fs.readFile(filePath);
+          const workbook = xlsx.read(buffer, { type: "buffer" });
+          const result: Record<string, unknown[]> = {};
 
           workbook.SheetNames.forEach((sheetName) => {
             const sheet = workbook.Sheets[sheetName];
