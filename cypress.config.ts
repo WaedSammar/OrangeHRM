029a0bc4320612148088fb7cf9e3a202d36dcb82
Mohammad Abohasan <mabohasan@apple.com>
2025-06-01 21:32:53 +0300

    refactor! remove viewportWidth/Height

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  projectId: "zwebt5",
+  projectId: 'zwebt5',
   e2e: {
-    baseUrl: "https://opensource-demo.orangehrmlive.com",
-    specPattern: "cypress/e2e/**/*-spec.cy.ts",
-    defaultBrowser: "chrome",
-  },
-  viewportWidth: 1728,
-  viewportHeight: 1117,
-});
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    specPattern: 'cypress/e2e/**/*-spec.cy.ts',
+    defaultBrowser: 'chrome',
+    video: false
+  }
+})
